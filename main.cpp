9123f71b5c7da64ed5f84ff58af98051f76e6983
langn0te <langn0te@langn0te-Ubuntu>
2014-10-03 15:02:20 -0300

    Second commit

---
+++ main.cpp
@@ -0,0 +1,20 @@
+/* 
+ * File:   main.cpp
+ * Author: langn0te
+ *
+ * Created on 3 de Outubro de 2014, 13:31
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    
+    return 0;
+}
+
